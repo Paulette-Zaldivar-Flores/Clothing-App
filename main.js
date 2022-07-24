4b72de2f7bce0039d49d90ff16178f17cbe3c5e2
Void321 <paulettz@icloud.com>
2022-07-24 18:15:10 +0900

    Updating products colors and cart

--- main.js
+++ main.js
@@ -3,13 +3,27 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Dress",
+      brand: "Pretty Dolls",
       image: "./assets/images/green-dress.jpeg",
       inStock: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/green-dress.jpeg" },
+        { id: 2235, color: "blue", image: "./assets/images/blue-dress.jpeg" },
       ],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+    computed: {
+      title() {
+        return this.brand + " " + this.product;
+      },
+    },
   },
 });
