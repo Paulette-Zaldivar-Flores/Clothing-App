9285a445972d74ec7e908f0e5ece00f3d56aab53
Void321 <paulettz@icloud.com>
2022-07-24 17:10:18 +0900

    Updating product info and cart in main.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Dress",
       image: "./assets/images/green-dress.jpeg",
-      inventory: 100,
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
